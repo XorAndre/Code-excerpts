1f716558beed66708593c41af5f13d4dacc817aa
XorAndre <xor2017@outlook.com>
2019-03-11 22:25:44 -0300

    Js corrigido

--- componentes/menu_responsivo/assets/js/main.js
+++ componentes/menu_responsivo/assets/js/main.js
@@ -2,7 +2,7 @@
 	document.getElementById('btn-menu').onclick = function() {
 		let verificaMenu = document.getElementById('menu').className;
 		if(verificaMenu == 'menu') {
-			document.getElementById('menu').className = 'menu links';
+			document.getElementById('menu').className = 'menu db';
 		} else {
 			document.getElementById('menu').className = 'menu';
 		}		
